06de2b65a890e8a0781c8c6eddd427a579c7a811
DiegoRomeroSantiago <dieromsan251005@gmail.com>
2024-11-08 14:36:27 +0100

    8-11.1

--- Assets/Scripts/bola.cs
+++ Assets/Scripts/bola.cs
@@ -7,6 +7,9 @@ public class bola : MonoBehaviour
 {
     [SerializeField] int fuerzaMovimiento;
     [SerializeField] int fuerzaSalto;
+    [SerializeField] float rayoSuelo;
+    float w;
+    float s;
     Rigidbody rb;
     void Start()
     {
@@ -15,13 +18,20 @@ public class bola : MonoBehaviour
     void Update()
     {
 
-        float w = Input.GetAxisRaw("Vertical");
-        float s = Input.GetAxisRaw("Horizontal");
+         w = Input.GetAxisRaw("Vertical");
+         s = Input.GetAxisRaw("Horizontal");
+
+        if (rb != null) 
+        {
+            salto();            
+        }
+
+
+    }
+    private void FixedUpdate()
+    {
         rb.AddForce(new Vector3(s, 0, w) * fuerzaMovimiento, ForceMode.Force );
-
-        salto();
-
-
+        
     }
     void salto()
     {
@@ -30,5 +40,13 @@ public class bola : MonoBehaviour
             rb.AddForce(new Vector3(0, 1, 0) * fuerzaSalto, ForceMode.Impulse);
 
         }
+    }
+
+    bool detectarsuelo()
+    {
+        bool tocasuelo = Physics.Raycast(gameObject.transform.position, Vector3.down, rayoSuelo);
+        return tocasuelo;
+
+        
     }
 }
--- Assets/Scripts/prefabrotador.cs
+++ Assets/Scripts/prefabrotador.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class prefabrotador : MonoBehaviour
 {
     [SerializeField] int fuerzaMovimiento;
+    [SerializeField] Vector3 dir;
     Rigidbody rb;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.AddTorque(dir * fuerzaMovimiento, ForceMode.VelocityChange);
     }
 
     
     void Update()
     {
-        rb.AddTorque(new Vector3(1, 1, 1) * fuerzaMovimiento, ForceMode.Force);
 
     }
 }
